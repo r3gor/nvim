a1159053c09784bfd2e214bcda74ff0cb9c668b5
Roger <rogrp6@gmail.com>
2023-10-22 00:41:51 -0500

    feat: add gitsigns plugin

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,12 @@
+return {
+	"lewis6991/gitsigns.nvim",
+	--config = function ()
+	--		require("gitsigns").setup()
+	--end
+	-- config = true -- Tambien se puede con esto en lugar de lo anterior, ya lazy se encarga
+	opts = {
+		signcolumn = false,
+		numhl = true,
+		max_file_length = 10000,
+	}
+}
