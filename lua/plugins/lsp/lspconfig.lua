041d227041a52c009ac41650e1cb1991a98f9cf7
Roger <rogrp6@gmail.com>
2023-11-16 04:46:00 -0500

    fix: improve angular lsp config (requires install angular language server as a dev dependency in proyect)

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -158,9 +158,22 @@ return {
       },
     })
 
+    local cwd = vim.fn.getcwd()
+    local cmd = {
+      cwd .. "/node_modules/.bin/ngserver",
+      "--stdio",
+      "--tsProbeLocations",
+      cwd .. "/node_modules",
+      "--ngProbeLocations",
+      cwd .. "/node_modules",
+    }
     lspconfig["angularls"].setup({
       capabilities = capabilities,
       on_attach = on_attach,
+      cmd = cmd,
+      on_new_config = function(new_config, new_root_dir)
+        new_config.cmd = cmd
+      end,
     })
   end,
 }
