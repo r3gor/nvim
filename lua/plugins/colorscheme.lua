21b70b6e19a13ae8c8ccce79eb04c0c8fdbd591d
Roger <rogrp6@gmail.com>
2023-11-16 04:45:19 -0500

    feat: new colorschemes, better compatility

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,12 +2,14 @@ return {
 	--"wuelnerdotexe/vim-enfocado",
 	--"fneu/breezy",
   --"nanotech/jellybeans.vim",
-  "bluz71/vim-moonfly-colors",
+  -- "bluz71/vim-moonfly-colors",
+  "lunarvim/colorschemes",
 	lazy=false,
 	priority = 1000,
 	config = function()
     vim.cmd([[set background=dark]])
     vim.cmd([[set termguicolors]])
-		vim.cmd([[colorscheme moonfly]])
+		-- vim.cmd([[colorscheme moonfly]])
+    vim.cmd([[colorscheme lunar]])
 	end,
 }
