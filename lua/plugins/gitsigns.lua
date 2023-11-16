93916d3aa1c43de7616170e5724d1cea8c665248
Roger <rogrp6@gmail.com>
2023-11-16 05:47:50 -0500

    fix: gitsigns current line blame in lunarvim colorschemes

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,12 +1,24 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	--config = function ()
-	--		require("gitsigns").setup()
-	--end
-	-- config = true -- Tambien se puede con esto en lugar de lo anterior, ya lazy se encarga
-	opts = {
-		-- signcolumn = false,
-		numhl = true,
-		max_file_length = 10000,
-	}
+  config = function()
+    require('gitsigns').setup({
+      -- signcolumn = false,
+      numhl = true,
+      max_file_length = 10000,
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+      },
+      current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+    })
+
+    -- fix problem with lunar colorschemes
+    vim.cmd([[
+      highlight GitSignsCurrentLineBlame guifg=#808080 ctermfg=gray guibg=NONE ctermbg=NONE
+      ]])
+  end,
 }
